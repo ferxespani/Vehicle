075e5cc36eb1c5a16cef9353b16a55b523639ff7
Vitaly <mickelangello45@gmail.com>
2018-11-25 08:54:04 +0200

    Rename a variable

--- Vehicle/Program.cs
+++ Vehicle/Program.cs
@@ -11,10 +11,10 @@ namespace Vehicle
         static void Main(string[] args)
         {
             Vehicle v = new Lorry("Mazda", 1998, 567, "gray", 122);
-            Vehicle d = new PassengerCar("Mazda", 1998, 455, "blue", 5);
+            Vehicle n = new PassengerCar("Mazda", 1998, 455, "blue", 5);
 
-            if (v.Equals(d))
-                Console.WriteLine($"This vehicles:  !{v}! and !{d}! are equal");
+            if (n.Equals(d))
+                Console.WriteLine($"This vehicles:  !{v}! and !{n}! are equal");
             else
                 Console.WriteLine("This vehicles are not equal");
         }
