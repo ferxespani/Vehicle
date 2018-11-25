3d0457fe216512697e11ff6bb32f9d806e03d8fd
Vitaly <mickelangello45@gmail.com>
2018-11-25 08:51:33 +0200

    Adds some objects

--- Vehicle/Program.cs
+++ Vehicle/Program.cs
@@ -10,6 +10,13 @@ namespace Vehicle
     {
         static void Main(string[] args)
         {
+            Vehicle v = new Lorry("Mazda", 1998, 567, "gray", 122);
+            Vehicle d = new PassengerCar("Mazda", 1998, 455, "blue", 5);
+
+            if (v.Equals(d))
+                Console.WriteLine($"This vehicles:  !{v}! and !{d}! are equal");
+            else
+                Console.WriteLine("This vehicles are not equal");
         }
     }
 }
